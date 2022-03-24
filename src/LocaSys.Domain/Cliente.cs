c772711625fccb787182bf88ab3b85a2c3d83c89
magno_barboza@hotmail.com <magno_barboza@hotmail.com>
2022-03-23 22:16:01 -0300

    Add project files.

---
+++ src/LocaSys.Domain/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaSys.Domain
+{
+    public class Cliente
+    {
+        public Cliente(int id, string nome, string cpf, DateTime dtNascimento)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentNullException("Nome é obrigatório.");
+
+            if (nome.Length > 200)
+                throw new ArgumentException("Nome não pode conter mais de 200 caracteres.");
+
+            if (string.IsNullOrEmpty(cpf))
+                throw new ArgumentNullException("Cpf é obrigatório.");
+
+            if (cpf.Length > 11)
+                throw new ArgumentException("Cpf inválido.");
+
+            if (dtNascimento == DateTime.MinValue)
+                throw new ArgumentException("Data inválida.");
+
+            Id = id;
+            Nome = nome;
+            CPF = cpf;
+            DtNascimento = dtNascimento;
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public DateTime DtNascimento { get; private set; }
+    }
+}
---
+++ tests/LocaSys.DomainTest/Clientes/ClienteTest.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using ExpectedObjects;
+using LocaSys.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LocaSys.DomainTest.Clientes
+{
+    public class ClienteTest: IDisposable
+    {
+        private int _id;
+        private string _nome;
+        private string _cpf;
+        private DateTime _dtNascimento;
+
+        public ClienteTest()
+        {
+            Faker faker = new Faker();
+
+            _id = 0;
+            _nome = $"{faker.Person.FirstName} {faker.Person.LastName}";
+            _cpf = faker.Random.ReplaceNumbers("###########");
+            _dtNascimento = faker.Person.DateOfBirth;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        [Fact]
+        public void DeveGravarCliente()
+        {
+            var clienteEsperado = new
+            {
+                Id = _id,
+                Nome = _nome,
+                CPF = _cpf,
+                DtNascimento = _dtNascimento
+            };
+
+            Cliente cliente = new Cliente(clienteEsperado.Id, clienteEsperado.Nome, clienteEsperado.CPF, clienteEsperado.DtNascimento);
+
+            clienteEsperado.ToExpectedObject().ShouldMatch(cliente);
+        }
+    }
+}
